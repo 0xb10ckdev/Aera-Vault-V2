77d9cb46b88696145ac331e158eeeae87094482a
astermiracle <astermiracle1009@gmail.com>
2023-03-23 20:10:58 +1000

    add test for initialize

---
+++ test/v2/AeraBalancerExecution/functions/Initialize.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "../TestBaseBalancerExecution.sol";
+
+contract InitializeTest is TestBaseBalancerExecution {
+    function setUp() public override {
+        vm.createSelectFork(vm.envString("ETH_NODE_URI_MAINNET"), 16826100);
+
+        _init();
+
+        _deployAssetRegistry();
+        _deployBalancerManagedPoolFactory();
+        _deployBalancerExecution();
+    }
+
+    function test_initialize_fail_whenModuleIsAlreadyInitialized() public {
+        for (uint256 i = 0; i < 3; i++) {
+            erc20Assets[i].approve(address(balancerExecution), 1);
+        }
+
+        balancerExecution.initialize(address(this));
+
+        vm.expectRevert(
+            AeraBalancerExecution.Aera__ModuleIsAlreadyInitialized.selector
+        );
+        balancerExecution.initialize(address(this));
+    }
+
+    function test_initialize_fail_whenVaultIsZeroAddress() public {
+        vm.expectRevert(
+            AeraBalancerExecution.Aera__VaultIsZeroAddress.selector
+        );
+        balancerExecution.initialize(address(0));
+    }
+
+    function test_initialize_fail_whenCallerIsNotOwner() public {
+        vm.startPrank(_USER);
+
+        vm.expectRevert(bytes("Ownable: caller is not the owner"));
+        balancerExecution.initialize(address(this));
+    }
+
+    function test_initialize_success() public {
+        for (uint256 i = 0; i < 3; i++) {
+            erc20Assets[i].approve(address(balancerExecution), 1);
+        }
+
+        balancerExecution.initialize(address(this));
+    }
+}
