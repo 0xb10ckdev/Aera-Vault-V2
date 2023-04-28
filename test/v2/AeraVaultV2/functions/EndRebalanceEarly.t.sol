d357cd6f506cfa44cc23805a5f6f1a907a483a78
astermiracle <astermiracle1009@gmail.com>
2023-04-28 13:27:00 +1000

    add base test for endRebalanceEarly of custody module

---
+++ test/v2/AeraVaultV2/functions/EndRebalanceEarly.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import "../../utils/TestBaseCustody/functions/EndRebalanceEarly.sol";
+import "../TestBaseAeraVaultV2.sol";
+
+contract EndRebalanceEarlyTest is
+    BaseEndRebalanceEarlyTest,
+    TestBaseAeraVaultV2
+{
+    function _generateRequest()
+        internal
+        override
+        returns (ICustody.AssetValue[] memory requests)
+    {
+        return _generateRequestWith3Assets();
+    }
+}
---
+++ test/v2/utils/TestBaseCustody/functions/EndRebalanceEarly.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import {ERC20Mock} from "../../../../utils/ERC20Mock.sol";
+import "../TestBaseCustody.sol";
+
+abstract contract BaseEndRebalanceEarlyTest is TestBaseCustody {
+    function test_endRebalanceEarly_fail_whenCallerIsNotOwnerOrGuardian()
+        public
+    {
+        vm.expectRevert(ICustody.Aera__CallerIsNotOwnerOrGuardian.selector);
+
+        vm.prank(_USER);
+        custody.endRebalanceEarly();
+    }
+
+    function test_endRebalanceEarly_fail_whenVaultIsPaused() public {
+        custody.pauseVault();
+
+        vm.expectRevert(ICustody.Aera__VaultIsPaused.selector);
+
+        custody.endRebalanceEarly();
+    }
+
+    function test_endRebalanceEarly_success_whenRebalancingIsOnGoing()
+        public
+        virtual
+    {
+        vm.prank(custody.guardian());
+        _startRebalance();
+
+        vm.expectEmit(true, true, true, true, address(custody));
+        emit EndRebalanceEarly();
+
+        custody.endRebalanceEarly();
+    }
+
+    function test_endRebalanceEarly_success() public virtual {
+        vm.prank(custody.guardian());
+        _startRebalance();
+
+        vm.warp(custody.execution().rebalanceEndTime());
+
+        vm.expectEmit(true, true, true, true, address(custody));
+        emit EndRebalanceEarly();
+
+        custody.endRebalanceEarly();
+    }
+}
