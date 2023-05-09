53aae7c1cfa7ba2de7accfc663eb7cb81a2e5b3f
astermiracle <astermiracle1009@gmail.com>
2023-05-10 02:14:44 +1000

    update sweep tests with fuzzing test

--- test/v2/utils/TestBaseCustody/functions/Sweep.sol
+++ test/v2/utils/TestBaseCustody/functions/Sweep.sol
@@ -5,22 +5,25 @@ import {ERC20Mock} from "../../../../utils/ERC20Mock.sol";
 import "../TestBaseCustody.sol";
 
 abstract contract BaseSweepTest is TestBaseCustody {
-    function test_sweep_success() public virtual {
+    function test_sweep_success_fuzzed(
+        uint256 balance,
+        uint256 amount
+    ) public virtual {
+        vm.assume(balance < type(uint256).max - amount);
+
         IERC20 erc20 = IERC20(
-            address(new ERC20Mock("Token", "TOKEN", 18, 1e30))
+            address(new ERC20Mock("Token", "TOKEN", 18, balance))
         );
-        deal(address(erc20), _USER, 10e18);
+        deal(address(erc20), _USER, amount);
 
         vm.prank(_USER);
-        erc20.transfer(address(custody), 10e18);
-
-        uint256 balance = erc20.balanceOf(address(this));
+        erc20.transfer(address(custody), amount);
 
         vm.expectEmit(true, true, true, true, address(custody));
-        emit Sweep(erc20, _ONE);
+        emit Sweep(erc20, amount);
 
-        custody.sweep(erc20, _ONE);
+        custody.sweep(erc20, amount);
 
-        assertEq(erc20.balanceOf(address(this)), balance + _ONE);
+        assertEq(erc20.balanceOf(address(this)), balance + amount);
     }
 }
--- test/v2/utils/TestBaseExecution/functions/Sweep.sol
+++ test/v2/utils/TestBaseExecution/functions/Sweep.sol
@@ -5,22 +5,25 @@ import {ERC20Mock} from "../../../../utils/ERC20Mock.sol";
 import "../TestBaseExecution.sol";
 
 abstract contract BaseSweepTest is TestBaseExecution {
-    function test_sweep_success() public virtual {
+    function test_sweep_success_fuzzed(
+        uint256 balance,
+        uint256 amount
+    ) public virtual {
+        vm.assume(balance < type(uint256).max - amount);
+
         IERC20 erc20 = IERC20(
-            address(new ERC20Mock("Token", "TOKEN", 18, 1e30))
+            address(new ERC20Mock("Token", "TOKEN", 18, balance))
         );
-        deal(address(erc20), _USER, 10e18);
+        deal(address(erc20), _USER, amount);
 
         vm.prank(_USER);
-        erc20.transfer(address(execution), 10e18);
-
-        uint256 balance = erc20.balanceOf(address(this));
+        erc20.transfer(address(execution), amount);
 
         vm.expectEmit(true, true, true, true, address(execution));
-        emit Sweep(erc20, _ONE);
+        emit Sweep(erc20, amount);
 
-        execution.sweep(erc20, _ONE);
+        execution.sweep(erc20, amount);
 
-        assertEq(erc20.balanceOf(address(this)), balance + _ONE);
+        assertEq(erc20.balanceOf(address(this)), balance + amount);
     }
 }
