52207250a7f8f98e7c9a3a22c55a991c49871a50
astermiracle <astermiracle1009@gmail.com>
2023-06-30 10:16:54 +1000

    update AeraVaultV2Factory with hooks

--- src/v2/AeraVaultV2Factory.sol
+++ src/v2/AeraVaultV2Factory.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.19;
 
 import "@openzeppelin/Ownable.sol";
 import "./interfaces/IAeraVaultV2Factory.sol";
+import "./AeraVaultHooks.sol";
 import "./AeraVaultV2.sol";
 
 /// @title Aera Vault V2 Factory contract.
@@ -11,16 +12,23 @@ contract AeraVaultV2Factory is IAeraVaultV2Factory, Ownable {
 
     /// @notice Emitted when the vault is created.
     /// @param vault Vault address.
+    /// @param hooks Hooks address.
     /// @param assetRegistry The address of asset registry.
     /// @param guardian The address of guardian.
     /// @param feeRecipient The address of fee recipient.
     /// @param fee Guardian fee per second in 18 decimal fixed point format.
+    /// @param maxDailyExecutionLoss  The fraction of value that the vault can
+    ///                                lose per day in the course of submissions.
+    /// @param targetSighashAllowlist Array of target sighash to allow.
     event VaultCreated(
         address vault,
+        address hooks,
         address assetRegistry,
         address guardian,
         address feeRecipient,
-        uint256 fee
+        uint256 fee,
+        uint256 maxDailyExecutionLoss,
+        TargetSighash[] targetSighashAllowlist
     );
 
     /// FUNCTIONS ///
@@ -30,7 +38,9 @@ contract AeraVaultV2Factory is IAeraVaultV2Factory, Ownable {
         address assetRegistry,
         address guardian,
         address feeRecipient,
-        uint256 fee
+        uint256 fee,
+        uint256 maxDailyExecutionLoss,
+        TargetSighash[] memory targetSighashAllowlist
     ) external override onlyOwner {
         AeraVaultV2 vault = new AeraVaultV2(
             assetRegistry,
@@ -38,10 +48,24 @@ contract AeraVaultV2Factory is IAeraVaultV2Factory, Ownable {
             feeRecipient,
             fee
         );
+
+        AeraVaultHooks hooks =
+        new AeraVaultHooks(address(vault), maxDailyExecutionLoss, targetSighashAllowlist);
+
+        vault.setHooks(address(hooks));
+
         vault.transferOwnership(msg.sender);
+        hooks.transferOwnership(msg.sender);
 
         emit VaultCreated(
-            address(vault), assetRegistry, guardian, feeRecipient, fee
+            address(vault),
+            address(hooks),
+            assetRegistry,
+            guardian,
+            feeRecipient,
+            fee,
+            maxDailyExecutionLoss,
+            targetSighashAllowlist
         );
     }
 }
--- src/v2/interfaces/IAeraVaultV2Factory.sol
+++ src/v2/interfaces/IAeraVaultV2Factory.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
+import {TargetSighash} from "../Types.sol";
+
 /// @title Interface for v2 vault factory.
 interface IAeraVaultV2Factory {
     /// @notice Create v2 vault.
@@ -8,10 +10,15 @@ interface IAeraVaultV2Factory {
     /// @param guardian The address of guardian.
     /// @param feeRecipient The address of fee recipient.
     /// @param fee Guardian fee per second in 18 decimal fixed point format.
+    /// @param maxDailyExecutionLoss  The fraction of value that the vault can
+    ///                                lose per day in the course of submissions.
+    /// @param targetSighashAllowlist Array of target sighash to allow.
     function create(
         address assetRegistry,
         address guardian,
         address feeRecipient,
-        uint256 fee
+        uint256 fee,
+        uint256 maxDailyExecutionLoss,
+        TargetSighash[] memory targetSighashAllowlist
     ) external;
 }
