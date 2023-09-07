b3877107bdc48166a6600ff9ebeb2e4810714234
astermiracle <astermiracle1009@gmail.com>
2023-09-07 21:36:14 +0900

    update sweep tests with amount parameter

--- test/v2/AeraVaultAssetRegistry/functions/Sweep.t.sol
+++ test/v2/AeraVaultAssetRegistry/functions/Sweep.t.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.21;
 import "../TestBaseAssetRegistry.sol";
 
 contract SweepTest is TestBaseAssetRegistry {
-    IERC20 erc20;
+    IERC20 public erc20;
 
     event Sweep(address token, uint256 amount);
 
@@ -19,20 +19,22 @@ contract SweepTest is TestBaseAssetRegistry {
         vm.prank(_USER);
 
         vm.expectRevert("Ownable: caller is not the owner");
-        assetRegistry.sweep(address(erc20));
+        assetRegistry.sweep(address(erc20), 1);
     }
 
     function test_sweep_success_withETH() public {
-        deal(address(assetRegistry), ONE);
+        deal(address(assetRegistry), ONE * 2);
 
         uint256 balance = address(this).balance;
+        uint256 assetRegistryBalance = address(assetRegistry).balance;
 
         vm.expectEmit(true, true, true, true, address(assetRegistry));
         emit Sweep(address(0), ONE);
 
-        assetRegistry.sweep(address(0));
+        assetRegistry.sweep(address(0), ONE);
 
         assertEq(address(this).balance, balance + ONE);
+        assertEq(address(assetRegistry).balance, assetRegistryBalance - ONE);
     }
 
     function test_sweep_success_withERC20() public {
@@ -40,13 +42,18 @@ contract SweepTest is TestBaseAssetRegistry {
         erc20.transfer(address(assetRegistry), 10e18);
 
         uint256 balance = erc20.balanceOf(address(this));
+        uint256 assetRegistryBalance = erc20.balanceOf(address(assetRegistry));
 
         vm.expectEmit(true, true, true, true, address(assetRegistry));
         emit Sweep(address(erc20), 10e18);
 
-        assetRegistry.sweep(address(erc20));
+        assetRegistry.sweep(address(erc20), 10e18);
 
         assertEq(erc20.balanceOf(address(this)), balance + 10e18);
+        assertEq(
+            erc20.balanceOf(address(assetRegistry)),
+            assetRegistryBalance - 10e18
+        );
     }
 
     receive() external payable {}
