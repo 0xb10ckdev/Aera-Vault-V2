de841074cf67ae5d59727a96177c43fbf7f2b46c
Ben <ben@benschreck.com>
2023-09-13 12:27:29 -0700

    Add modules to transfer ownership:

--- script/v2/TransferOwnership.s.sol
+++ script/v2/TransferOwnership.s.sol
@@ -2,15 +2,22 @@
 pragma solidity ^0.8.21;
 
 import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
 import "src/v2/AeraVaultV2.sol";
+import "src/v2/AeraVaultHooks.sol";
+import "src/v2/AeraVaultAssetRegistry.sol";
 
 contract TransferOwnership is Script {
     function run() public {
         address vaultAddress = vm.envAddress("VAULT_ADDRESS");
         address newOwner = vm.envAddress("NEW_OWNER");
         AeraVaultV2 vault = AeraVaultV2(payable(vaultAddress));
+        AeraVaultHooks hooks = AeraVaultHooks(address(vault.hooks()));
+        AeraVaultAssetRegistry assetRegistry = AeraVaultAssetRegistry(address(vault.assetRegistry()));
         vm.startBroadcast();
         vault.transferOwnership(newOwner);
+        hooks.transferOwnership(newOwner);
+        assetRegistry.transferOwnership(newOwner);
         vm.stopBroadcast();
     }
 }
