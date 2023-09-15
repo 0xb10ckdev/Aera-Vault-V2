afebf2a07c6715dae29ab716eeaf9551462165d8
Ben <ben@benschreck.com>
2023-09-15 12:58:03 -0700

    fix wsteth mainnet addr

--- script/v2/deploy/DeployAeraContracts.s.sol
+++ script/v2/deploy/DeployAeraContracts.s.sol
@@ -57,7 +57,7 @@ contract DeployAeraContracts is DeployScriptBase {
     address weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address usdcOracle = 0x986b5E1e1755e3C2440e960477f25201B0a8bbD4;
-    address wsteth = 0x03b54A6e9a984069379fae1a4fC4dBAE93B3bCCD;
+    address wsteth = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
     address waUSDC = 0x2F79D4CEB79EBD26161e51ca0C9300F970DEd54d;
     address uniswapSwapRouter = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
 
