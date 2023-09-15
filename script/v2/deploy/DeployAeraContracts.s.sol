35a6164ba356fdca07644bca91edeb0bdc0e1df1
Ben <ben@benschreck.com>
2023-09-15 12:03:37 -0700

    non json target sighash deploy

--- script/v2/deploy/DeployAeraContracts.s.sol
+++ script/v2/deploy/DeployAeraContracts.s.sol
@@ -11,6 +11,8 @@ import {AeraVaultHooks} from "src/v2/AeraVaultHooks.sol";
 import {AeraVaultV2} from "src/v2/AeraVaultV2.sol";
 import {AeraV2Factory} from "src/v2/AeraV2Factory.sol";
 import {IAssetRegistry} from "src/v2/interfaces/IAssetRegistry.sol";
+import "@openzeppelin/IERC4626.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import {
     TargetSighash,
     TargetSighashData,
@@ -21,8 +23,62 @@ import {
 import {DeployScriptBase} from "script/utils/DeployScriptBase.sol";
 import "@chainlink/interfaces/AggregatorV2V3Interface.sol";
 
+interface CurvePool {
+    function coins(uint256 i) external view returns (address);
+
+    function price_oracle() external view returns (uint256);
+
+    function get_dy(
+        uint256 i,
+        uint256 j,
+        uint256 dx
+    ) external view returns (uint256);
+
+    function exchange(
+        uint256 i,
+        uint256 j,
+        uint256 dx,
+        uint256 min_dy
+    ) external returns (uint256);
+}
+
 contract DeployAeraContracts is DeployScriptBase {
     using stdJson for string;
+
+    TargetSighashData[] targetSighashAllowlist;
+    address wethPolygon = 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619;
+    address usdcPolygon  = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
+    address waPolWETH = 0x5e5057b8D220eb8573Bc342136FdF1d869316D18;
+    address wstethPolygon  = 0x03b54A6e9a984069379fae1a4fC4dBAE93B3bCCD;
+    address swapRouterAddressPolygon  = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+
+    address teth = 0x752eBeb79963cf0732E9c0fec72a49FD1DEfAEAC;
+    address T = 0xCdF7028ceAB81fA0C6971208e83fa7872994beE5;
+    address weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address usdcOracle = 0x986b5E1e1755e3C2440e960477f25201B0a8bbD4;
+    address wsteth = 0x03b54A6e9a984069379fae1a4fC4dBAE93B3bCCD;
+    address waUSDC = 0x2F79D4CEB79EBD26161e51ca0C9300F970DEd54d;
+    address uniswapSwapRouter = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+
+    address[] whitelistedCurveTargets;
+    address[] internal whitelistedCurveTargetsMainnet = [
+        teth
+    ];
+    address[] whitelistedERC20Targets;
+    address[] internal whitelistedERC20TargetsMainnet = [
+        wsteth,
+        weth,
+        usdc
+    ];
+    address[] whitelistedERC4626Targets;
+    address[] internal whitelistedERC4626TargetsMainnet = [
+        waUSDC
+    ];
+    address[] whitelistedSwapRouters;
+    address[] internal whitelistedSwapRoutersMainnet = [
+        uniswapSwapRouter
+    ];
 
     /// @notice Deploy AssetRegistry, AeraVaultV2 and Hooks if they were not
     ///         deployed yet.
@@ -228,27 +284,95 @@ contract DeployAeraContracts is DeployScriptBase {
         address owner = json.readAddress(".owner");
         uint256 minDailyValue = json.readUint(".minDailyValue");
 
-        bytes32[] memory allowlistRaw =
-            json.readBytes32Array(".targetSighashAllowlist");
-        TargetSighash[] memory allowlist;
-        assembly {
-            allowlist := allowlistRaw
-        }
+        //bytes32[] memory allowlistRaw =
+        //    json.readBytes32Array(".targetSighashAllowlist");
+        //TargetSighash[] memory allowlist;
+        //assembly {
+        //    allowlist := allowlistRaw
+        //}
 
-        TargetSighashData[] memory targetSighashAllowlist =
-            new TargetSighashData[](allowlist.length);
-        for (uint256 i = 0; i < allowlist.length; i++) {
-            targetSighashAllowlist[i] = TargetSighashData({
-                target: _getTarget(allowlist[i]),
-                selector: _getSelector(allowlist[i])
-            });
+        //for (uint256 i = 0; i < allowlist.length; i++) {
+        //    targetSighashAllowlist[i] = TargetSighashData({
+        //        target: _getTarget(allowlist[i]),
+        //        selector: _getSelector(allowlist[i])
+        //    });
+        //}
+        if (this.getChainID() == 137) {
+            //whitelistedCurveTargets = whitelistedCurveTargetsPolygon;
+            //whitelistedERC20Targets = whitelistedERC20TargetsPolygon;
+            //whitelistedERC4626Targets = whitelistedERC4626TargetsPolygon;
+            //whitelistedSwapRouters = whitelistedSwapRoutersPolygon;
+        } else if (this.getChainID() == 1) {
+            whitelistedCurveTargets = whitelistedCurveTargetsMainnet;
+            whitelistedERC20Targets = whitelistedERC20TargetsMainnet;
+            whitelistedERC4626Targets = whitelistedERC4626TargetsMainnet;
+            whitelistedSwapRouters = whitelistedSwapRoutersMainnet;
+        } else {
+            revert("unsupported chain");
+        }
+        for (uint256 i = 0; i < whitelistedCurveTargets.length; i++) {
+            targetSighashAllowlist.push(TargetSighashData({
+                target: whitelistedCurveTargetsMainnet[i],
+                selector: CurvePool.exchange.selector
+            }));
+        }
+        for (uint256 i = 0; i < whitelistedERC20Targets.length; i++) {
+            targetSighashAllowlist.push(TargetSighashData({
+                target: whitelistedERC20Targets[i],
+                selector: IERC20.approve.selector
+            }));
+        }
+        for (uint256 i = 0; i < whitelistedERC4626Targets.length; i++) {
+            targetSighashAllowlist.push(TargetSighashData({
+                target: whitelistedERC4626Targets[i],
+                selector: IERC20.approve.selector
+            }));
+            targetSighashAllowlist.push(TargetSighashData({
+                target: whitelistedERC4626Targets[i],
+                selector: IERC4626.deposit.selector
+            }));
+            targetSighashAllowlist.push(TargetSighashData({
+                target: whitelistedERC4626Targets[i],
+                selector: IERC4626.withdraw.selector
+            }));
+            targetSighashAllowlist.push(TargetSighashData({
+                target: whitelistedERC4626Targets[i],
+                selector: IERC4626.mint.selector
+            }));
+            targetSighashAllowlist.push(TargetSighashData({
+                target: whitelistedERC4626Targets[i],
+                selector: IERC4626.redeem.selector
+            }));
+        }
+        for (uint256 i = 0; i < whitelistedSwapRouters.length; i++) {
+            targetSighashAllowlist.push(TargetSighashData({
+                target: whitelistedSwapRouters[i],
+                selector: ISwapRouter.exactInput.selector
+            }));
+            targetSighashAllowlist.push(TargetSighashData({
+                target: whitelistedSwapRouters[i],
+                selector: ISwapRouter.exactInputSingle.selector
+            }));
+            targetSighashAllowlist.push(TargetSighashData({
+                target: whitelistedSwapRouters[i],
+                selector: ISwapRouter.exactOutput.selector
+            }));
+            targetSighashAllowlist.push(TargetSighashData({
+                target: whitelistedSwapRouters[i],
+                selector: ISwapRouter.exactOutputSingle.selector
+            }));
+        }
+        TargetSighashData[] memory targetSighashAllowlistMem =
+            new TargetSighashData[](targetSighashAllowlist.length);
+        for (uint256 i = 0; i < targetSighashAllowlist.length; i++) {
+            targetSighashAllowlistMem[i] = targetSighashAllowlist[i];
         }
 
         return HooksParameters(
             factory,
             owner == address(0) ? _deployerAddress : owner,
             minDailyValue,
-            targetSighashAllowlist
+            targetSighashAllowlistMem
         );
     }
 
@@ -355,4 +479,12 @@ contract DeployAeraContracts is DeployScriptBase {
 
         console.log("Checked Hooks Integrity");
     }
+    function getChainID() external view returns (uint256) {
+        uint256 id;
+        assembly {
+            id := chainid()
+        }
+        return id;
+    }
+
 }
