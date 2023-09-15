d3027b001995c0f9853ad4c72b5e99041f458843
Ben <ben@benschreck.com>
2023-09-15 13:37:42 -0700

    address comments

--- periphery/CurveOracleWrapper.sol
+++ periphery/CurveOracleWrapper.sol
@@ -2,16 +2,39 @@
 pragma solidity 0.8.21;
 import "@openzeppelin/IERC20Metadata.sol";
 import "./ICurveFiPool.sol";
-import "./IAeraV2Oracle.sol";
 
-contract CurveOracleWrapper is IAeraV2Oracle {
-    address public pool;
-    uint256 public numeraireIndex;
-    uint256 public numeraireDecimals;
-    uint256 public tokenToPriceIndex;
-    uint256 public decimals_;
+/// @title CurveOracleWrapper 
+/// @notice Used to calculate price of tokens in a Curve V2 pool
+contract CurveOracleWrapper is AggregatorV2V3Interface {
+    /// @notice The address of underlying curve pool
+    address public immutable pool;
+
+    /// @notice The index in the curve pool of the numeraire asset to price against
+    uint256 public immutable numeraireIndex;
+
+    /// @notice 10 ** decimals of numeraire asset
+    uint256 public immutable numeraireScale;
+
+    /// @notice The index in the curve pool of the asset we want a price for
+    uint256 public immutable tokenToPriceIndex;
+
+    /// @notice Decimals of price returned by this oracle
+    uint256 public immutable decimals;
+
+    /// ERRORS ///
+
+    error TokenToPriceNotFoundInPoool(address pool, address tokenToPrice);
+    error NumeraireTokenNotFoundInPool(address pool, address numeraireToken);
+    error CurvePriceOverflowsInt256();
+
+    /// FUNCTIONS ///
+
+    /// @notice Initialize the oracle contract.
+    /// @param pool_ The address of the underlying curve pool
+    /// @param tokenToPrice_ The address of the underlying token to get a price for
+    /// @param numeraireToken The address of the other token in the pool to price against
     constructor(address pool_, address tokenToPrice, address numeraireToken) {
-        pool = pool_;
+        // Effects: find numeraire and token to price.
         for (uint256 i = 0; i < 2; i++) {
             if (ICurveFiPool(pool).coins(i) == numeraireToken) {
                 numeraireIndex = i;
@@ -19,24 +42,28 @@ contract CurveOracleWrapper is IAeraV2Oracle {
                 tokenToPriceIndex = i;
             }
         }
+
+        // Requirements: check that token to price is part of pool.
         if (tokenToPriceIndex >= 2) {
-            revert("tokenToPrice not found in pool");
+            revert TokenToPriceNotFoundInPoool(pool_, tokenToPrice);
         }
+
+        // Requirements: check that numeraire is part of pool.
         if (numeraireIndex >= 2) {
-            revert("numeraireToken not found in pool");
+            revert NumeraireTokenNotFoundInPool(pool_, numeraireToken);
         }
-        numeraireDecimals = IERC20Metadata(numeraireToken).decimals();
-        decimals_ = IERC20Metadata(tokenToPrice).decimals();
+        
+        // Effects: initialize contract variables.
+        pool = pool_;
+        numeraireScale = 10 ** IERC20Metadata(numeraireToken).decimals();
+        decimals = IERC20Metadata(tokenToPrice).decimals();
     }
     
-    function decimals() external view override returns (uint256) {
-        return decimals_;
-    }
-
+    /// @inheritdoc AggregatorV2V3Interface
     function latestRoundData() external view override returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) {
-        uint256 uintAnswer = ICurveFiPool(pool).get_dy(numeraireIndex, tokenToPriceIndex, 10**numeraireDecimals);
-        if (uintAnswer > 2**255) {
-            revert("overflow");
+        uint256 uintAnswer = ICurveFiPool(pool).get_dy(numeraireIndex, tokenToPriceIndex, numeraireScale);
+        if (uintAnswer > type(int256).max) {
+            revert CurvePriceOverflowsInt256();
         }
         answer = int256(uintAnswer);
 
