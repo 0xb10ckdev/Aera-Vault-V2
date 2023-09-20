852b450a4c961b83b6b956ca2e6d238a1daaf234
Julius Degesys <julius.degesys@gmail.com>
2023-09-20 19:13:58 -0400

    Have our Oracles inherit from IAeraV2Oracle

--- periphery/WstETHOracle.sol
+++ periphery/WstETHOracle.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.21;
+
 import "@openzeppelin/IERC20Metadata.sol";
-import "@chainlink/interfaces/AggregatorV2V3Interface.sol";
 import "./IWstETH.sol";
+import "./IAeraV2Oracle.sol";
 import {ONE} from "src/v2/Constants.sol";
 
 /// @title WstETHOracle
 /// @notice Used to calculate price of wstETH.
-contract WstETHOracle is AggregatorV2V3Interface {
+contract WstETHOracle is IAeraV2Oracle {
     /// @notice The address of wstETH.
     address public immutable wstETH;
 
@@ -17,7 +18,6 @@ contract WstETHOracle is AggregatorV2V3Interface {
     /// ERRORS ///
 
     error PriceOverflowsInt256();
-    error NotImplemented();
 
     /// FUNCTIONS ///
 
@@ -28,9 +28,20 @@ contract WstETHOracle is AggregatorV2V3Interface {
         wstETH = wstETH_;
         decimals = 18;
     }
-    
-    /// @inheritdoc AggregatorV3Interface
-    function latestRoundData() external view override returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) {
+
+    /// @inheritdoc IAeraV2Oracle
+    function latestRoundData()
+        external
+        view
+        override
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        )
+    {
         // Assumes ETH : stETH exchange rate of 1 : 1.
         // In future, can use stETH/ETH chainlink oracle to be slightly more accurate
         uint256 uintAnswer = IWstETH(wstETH).getStETHByWstETH(ONE);
@@ -41,50 +52,9 @@ contract WstETHOracle is AggregatorV2V3Interface {
         answer = int256(uintAnswer);
 
         updatedAt = block.timestamp;
-        
+
         roundId = 0;
         startedAt = 0;
         answeredInRound = 0;
-    }
-    
-    /// @inheritdoc AggregatorV3Interface
-    function description() external pure returns (string memory) {
-        return "";
-    }
-
-    function getAnswer(uint256) external pure returns (int256) {
-        revert NotImplemented();
-    }
-
-    function getRoundData(uint80) 
-        external pure 
-        returns (
-          uint80,
-          int256,
-          uint256,
-          uint256,
-          uint80
-        ) {
-        revert NotImplemented();
-    }
-
-    function getTimestamp(uint256) external pure returns (uint256) {
-        revert NotImplemented();
-    }
-
-    function latestAnswer() external pure returns (int256) {
-        revert NotImplemented();
-    }
-
-    function latestRound() external pure returns (uint256) {
-        revert NotImplemented();
-    }
-
-    function latestTimestamp() external pure returns (uint256) {
-        revert NotImplemented();
-    }
-
-    function version() external pure returns (uint256){
-        revert NotImplemented();
     }
 }
